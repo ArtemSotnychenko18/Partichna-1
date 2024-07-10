b8dd96d4b8f31aa014ea0b71779b26baa54042bc
ArtemSotnychenko18 <kn23-a.sotnychenko@nubip.edu.ua>
2024-07-10 17:39:13 +0300

    Update main.c

---
+++ практична 1/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    double t1, t2, t3;
+    
+    printf("Введіть три значення:\n");
+    scanf("%lf %lf %lf", &t1, &t2, &t3);
+    
+    double total_time = 1.0 / (1.0 / t1 + 1.0 / t2 + 1.0 / t3);
+    
+    printf("Час, необхідний для з'їдання пирога: %.2f годин\n", total_time);
+    
+    return 0;
+}
